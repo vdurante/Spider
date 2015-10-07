238a26a9ad3457e72e3043c885a27f22b245e45f
Vitor Durante <5401232+vdurante@users.noreply.github.com>
2015-10-06 22:04:45 -0300

    Implementar método de exibição dos resultados obtidos das máquinas

--- projeto1/backend.py
+++ projeto1/backend.py
@@ -46,7 +46,7 @@ class machine_thread (threading.Thread):
 
         threading.Thread.__init__(self)
         self.thread_id = thread_id
-        self.machine_id = (thread_id-1)%len(machines)
+        self.machine_id = (thread_id - 1) % len(machines)
         self.commands = commands
         self.q = q
 
@@ -106,7 +106,8 @@ class machine_thread (threading.Thread):
                     results["results"][index]["response"] = response[2]
                 else:
                     # caso contrario, algum erro ocorreu
-                    results["results"][index]["response"] = "Erro: algo ocorreu"
+                    results["results"][index][
+                        "response"] = "Erro: algo ocorreu"
 
             # fecha o socket apos todos os comandos serem enviados
             clientSocket.close
@@ -125,10 +126,87 @@ class machine_thread (threading.Thread):
 
 # metodo para printar os resultados obtidos de uma máquina
 def print_results(result):
-    print "TODO"
+    print "<div style='border: 1px solid #000; display:inline-block;margin:10px;padding:10px;vertical-align:top;'>"
+
+    # exibe o id linear da maquina
+    print "<h1>Maquina " + result["machine_id"] + "</h1>"
+
+    # exibe o IP e porta, para facilitar visualização
+    print "<h4>Servidor: " + result["server"]["IP"] + ":" + result["server"]["port"] + "</h3>"
+
+    if result["success"]:
+        # se o socket conectou-se ao daemon
+        for r in result["results"]:
+            # exibe o comando requerido
+            print "<h3>Comando: " + r["command"] + "</h3>"
+            print "<pre style='margin-left:15px'>"
+
+            # exibe a resposta
+            print r["response"]
+            print "</pre>"
+    else:
+        # caso contrario, conexão foi mal sucedida
+        print "Conexão mal sucedida"
+
+    print "</div>"
 
 if method == "POST":
 
+    # fila usada para armazenar os resultados dos comandos de cada máquina (thread)
+    # utiliza-se uma Queue porque é indicada para programas multi-thread,
+    # já que ela possui métodos de sincronização já implementados,
+    # garantindo segurança na troca e acesso de informação entre threads
+    # executando em paralelo
+    queue = Queue.Queue()
+
+    # queue_lock, usado para sincronização de escrita dentro da thread
+    queue_lock = threading.Lock()
+
+    # adquirir dados POST vindos da form presente na página anterior
+    form = cgi.FieldStorage()
+    data = json.loads(form.getvalue("data"))["machines"]
+
+    # array para as K threads que forem criadas (uma por máquina)
+    threads = []
+
+    for (index, machine) in enumerate(data):
+        # para cada máquina requerida, cria uma thread e passa como argumento
+        # index + 1 - identificador linear de cada thread - correspondente à contagem de Máquinas vinda do front-end
+        # array de comandos da máquina desejada
+        # queue para ser usado dentro da thread
+        thread = machine_thread(index + 1, machine["commands"], queue)
+        thread.start()
+
+        # thread criada adicionada ao array de threads
+        threads.append(thread)
+
+    for t in threads:
+        # sincronização - aguarda todads as threads encerrarem
+        t.join()
+
+    # cria array vazio com K posições (número de máquinas)
+    # será usado para ordernar as respostas existentes na queue
+    # a queue é preenchida em ordem de término de threads
+    # a queue não está ordenada por contagem de máquina
+    results = [None] * queue.qsize()
+
+    while not queue.empty():
+        r = queue.get()
+        # machine_id é único e linear
+        results[int(r["machine_id"]) - 1] = r
+
+    for r in results:
+        # para cada resultado das máquinas, imprime usando função print_results
+        print_results(r)
+
+    # ALTERNATIVA
+    # printar em ordem de término das threads, para verificar funcionamento do
+    # paralelismo
+
+    '''while not queue.empty():
+        # enquanto a queue não estiver vazia
+        # extrai primeiro elemento da lista a imprime ele usando a função de print_results
+        print_results(queue.get())'''
 
 else:
     # caso método não seja POST, negar acesso
