08af3f7766638a099374c3c19a231cc74c474bb2
Pamela Moreno Martinez <pamps2@gmail.com>
2015-12-08 20:08:58 -0200

    Implementacao do metodo checksum

--- projeto2/modules/udpi/__init__.py
+++ projeto2/modules/udpi/__init__.py
@@ -287,9 +287,24 @@ class udpi:
     # abrange little endian e big endian
     if struct.pack("H", 1) == "\x00\x01":
         def checksum(self, pkt):
-
+            pkt = str(pkt)
+            if len(pkt) % 2 == 1:
+                pkt += "\0"
+            s = sum(array.array("H", pkt))
+            s = (s >> 16) + (s & 0xffff)
+            s += s >> 16
+            s = ~s
+            return s & 0xffff
     else:
         def checksum(self, pkt):
+            pkt = str(pkt)
+            if len(pkt) % 2 == 1:
+                pkt += "\0"
+            s = sum(array.array("H", pkt))
+            s = (s >> 16) + (s & 0xffff)
+            s += s >> 16
+            s = ~s
+            return (((s >> 8) & 0xff) | s << 8) & 0xffff
 
 
     # metodo verify_checksum
